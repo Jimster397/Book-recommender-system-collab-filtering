6d1dedebab4525e3766f2036a8cb5503368052ca
James Schmidt <your_email@example.com>
2026-01-20 20:15:42 -0800

    Setup initialized

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="books_recommender",
+    version="0.0.1",
+    author="James Schmidt",
+    packages=find_packages(),
+    install_requires=[
+        # your dependencies
+    ]
+)
