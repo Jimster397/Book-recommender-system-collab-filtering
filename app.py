c4148d258a6060d1c309d506d898a5c5d404cf89
James Schmidt <your_email@example.com>
2026-01-21 20:54:31 -0800

    Add data ingestion and constant

--- app.py
+++ app.py
@@ -3,8 +3,4 @@ import sys
 import logging
 from books_recommender.logger import log
 
-try:
-    a = 3/0
-except Exception as e:
-    logging.info(e)
-    raise AppException(e, sys) from e
+import pandas as pd
