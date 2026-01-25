2466c53f9f8eb4b15bae1aa7e05755691e53fad1
James Schmidt <your_email@example.com>
2026-01-24 20:16:50 -0800

    Data ingestion added, pipeline configured

---
+++ books_recommender/components/stage_00_data_ingestion.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import urllib.request
+from books_recommender.logger import log
+import logging
+from books_recommender.exception.exception_handler import AppException
+from books_recommender.config.configuration import AppConfiguration
+
+
+class DataIngestion:
+    def __init__(self, app_config = AppConfiguration()):
+        """
+        Data Ingestion Initialization
+        data_ingestion_config: DataIngestionConfig
+        """
+        try:
+            logging.info(f"{'='*20}Data ingestion log started.{'='*20}")
+            self.data_ingestion_config = app_config.get_data_ingestion_config()
+        except Exception as e:
+            raise AppException(e, sys) from e
+    
+
+
+    def download_data(self):
+        """
+        Download all data files (books, ratings, users) from GitHub
+        """
+        try:
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+            os.makedirs(raw_data_dir, exist_ok=True)
+            
+            downloaded_files = {}
+            
+            # Download each CSV file
+            for file_type, url in self.data_ingestion_config.source_urls.items():
+                file_name = f"BX-{file_type.capitalize()}.csv"
+                file_path = os.path.join(raw_data_dir, file_name)
+                
+                logging.info(f"Downloading {file_type} data from {url}")
+                urllib.request.urlretrieve(url, file_path)
+                logging.info(f"Downloaded {file_type} data to {file_path}")
+                
+                downloaded_files[file_type] = file_path
+            
+            logging.info(f"All data files downloaded successfully")
+            return downloaded_files
+            
+        except Exception as e:
+            raise AppException(e, sys) from e
+        
+
+    def initiate_data_ingestion(self):
+        try:
+            downloaded_files = self.download_data()
+            logging.info(f"{'='*20}Data Ingestion log completed.{'='*20} \n\n")
+            return downloaded_files
+        except Exception as e:
+            raise AppException(e, sys) from e
--- books_recommender/config/configuration.py
+++ books_recommender/config/configuration.py
@@ -1,12 +1,11 @@
 import os 
 import sys
 import logging
-
-from numpy import e
 from books_recommender.logger import log
+from books_recommender.constant import *
 from books_recommender.utils.util import read_yaml_file
 from books_recommender.exception.exception_handler import AppException
-from books_recommender.constant import *
+from books_recommender.entity.config_entity import DataIngestionConfig
 
 
 
@@ -19,22 +18,22 @@ class AppConfiguration:
         
 
     
-def get_data_ingestion_config(self) -> DataIngestionConfig:
-    try:
-        # Get the data ingestion section from config
-        config = self.configs_info['data_ingestion_config']
-    
-        # Create the config object directly from the YAML values
-        response = DataIngestionConfig(
-            source_urls=config['source_urls'],
-            root_dir=config['root_dir'],
-            local_data_dir=config['local_data_dir'],
-            raw_data_dir=config['raw_data_dir'],
-            ingested_dir=config['ingested_dir']
-        )
-
-        logging.info(f"Data Ingestion Config: {response}")
-        return response
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        try:
+            # Get the data ingestion section from config
+            config = self.configs_info['data_ingestion_config']
         
-    except Exception as e:
-        raise AppException(e, sys) from e
+            # Create the config object directly from the YAML values
+            response = DataIngestionConfig(
+                source_urls=config['source_urls'],
+                root_dir=config['root_dir'],
+                local_data_dir=config['local_data_dir'],
+                raw_data_dir=config['raw_data_dir'],
+                ingested_dir=config['ingested_dir']
+            )
+
+            logging.info(f"Data Ingestion Config: {response}")
+            return response
+            
+        except Exception as e:
+            raise AppException(e, sys) from e
